5f4aa861b60c365ec191e064015432eec93fa080
Francisco Jaime <artisticpako@gmail.com>
2022-02-28 03:28:00 -0600

    Ultimo commit, todo funciona

--- app/src/main/java/com/example/practica1/MainActivity.kt
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -25,10 +25,13 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
         binding.etVarA.visibility = View.INVISIBLE
         binding.etVarB.visibility = View.INVISIBLE
         binding.btnCalcular.visibility = View.INVISIBLE
+        binding.tvDescription.text = resources.getString(R.string.description_initial)
 
         val lista = resources.getStringArray(R.array.opciones_spinner)
         val adaptador = ArrayAdapter(this,R.layout.items_lista,lista)
 
+        //Expose Drop-Down Menu para reemplazar el spinner
+        //Fuente: https://www.youtube.com/watch?v=isZ1vrOuG2o
         with(binding.tilSpinnerOptions){
             setAdapter(adaptador)
             onItemClickListener = this@MainActivity
@@ -47,6 +50,7 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
                 binding.ivFormula.setImageResource(R.drawable.formula1)
                 binding.etVarA.hint = resources.getString(R.string.formula1_var1)
                 binding.etVarB.hint = resources.getString(R.string.formula1_var2)
+                binding.tvDescription.text = resources.getString(R.string.description_cilindro)
             }
             1 ->{
                 binding.etVarA.visibility = View.VISIBLE
@@ -55,6 +59,7 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
                 binding.ivFormula.setImageResource(R.drawable.formula2)
                 binding.etVarA.hint = resources.getString(R.string.formula2_var1)
                 binding.etVarB.hint = resources.getString(R.string.formula2_var2)
+                binding.tvDescription.text = resources.getString(R.string.description_cono)
             }
             2 ->{
                 binding.etVarA.visibility = View.VISIBLE
@@ -64,8 +69,16 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
                 //binding.etVarA.setText(resources.getString(R.string.formula3_var1))
                 binding.etVarA.hint = resources.getString(R.string.formula3_var1)
                 binding.etVarB.hint = resources.getString(R.string.formula3_var2)
+                binding.tvDescription.text = resources.getString(R.string.description_prisma)
             }
         }
+    }
+
+    private fun validaCampos(): Boolean{
+        if( binding.etVarA.text.toString() == "" || binding.etVarB.text.toString() == "" ) return false
+        if( binding.etVarA.text.toString() == "" && binding.etVarB.text.toString() != "" ) return false
+        if( binding.etVarA.text.toString() != "" && binding.etVarB.text.toString() == "" ) return false
+        else return true
     }
 
     fun click(view: View) {
@@ -75,10 +88,10 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
 
             when(item_position){
                 0 ->{
-                    resultado = ((3.141539F*(var1*var1).toFloat())*var2.toFloat())
+                    resultado = ((3.141592F*(var1*var1).toFloat())*var2.toFloat())
                 }
                 1 ->{
-                    resultado = ((3.141539F*var1.toFloat())*var2.toFloat())
+                    resultado = ((3.141592F*var1.toFloat())*var2.toFloat())
                 }
                 2 ->{
                     resultado = (var1.toFloat()*var2.toFloat())
@@ -102,14 +115,4 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
         }
 
     }
-
-    private fun validaCampos(): Boolean{
-        if( binding.etVarA.text.toString() == "" || binding.etVarB.text.toString() == "" ) return false
-        if( binding.etVarA.text.toString() == "" && binding.etVarB.text.toString() != "" ) return false
-        if( binding.etVarA.text.toString() != "" && binding.etVarB.text.toString() == "" ) return false
-        else return true
-    }
-
-
-
 }
